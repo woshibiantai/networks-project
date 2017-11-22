d49c10ee88e8a66eaf7bc418cc40cb79d2cbd98b
jonbeibeibei <jonathanbeiqiyang@gmail.com>
2017-11-22 08:23:22 +0800

    Added Python3 support for GUI, using PyQt5

--- OutFile_ui_Python3.py
+++ OutFile_ui_Python3.py
@@ -6,7 +6,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import *
 import sys
 import json
 
@@ -22,11 +23,11 @@ try:
     	return QtGui.QApplication.translate(context, text, disambig, _encoding)
 except AttributeError:
     def _translate(context, text, disambig):
-    	return QtGui.QApplication.translate(context, text, disambig)
+    	return QApplication.translate(context, text, disambig)
 
-class Ui_Dialog(QtGui.QWidget):
+class Ui_Dialog(QWidget):
 	def __init__(self):
-		QtGui.QWidget.__init__(self)
+		QWidget.__init__(self)
 		self.setupUi(self)
 	def setupUi(self, Dialog):
 		Dialog.setObjectName(_fromUtf8("Dialog"))
@@ -43,20 +44,20 @@ class Ui_Dialog(QtGui.QWidget):
 			"selection-color: rgb(255, 170, 255);\n"
 			""))
 		Dialog.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
-		self.reset_btn = QtGui.QPushButton(Dialog)
+		self.reset_btn = QPushButton(Dialog)
 		self.reset_btn.setGeometry(QtCore.QRect(215, 420, 70, 23))
 		self.reset_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.reset_btn.setObjectName(_fromUtf8("reset_btn"))
-		self.apply_btn = QtGui.QPushButton(Dialog)
+		self.apply_btn = QPushButton(Dialog)
 		self.apply_btn.setGeometry(QtCore.QRect(215, 450, 70, 23))
 		self.apply_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.apply_btn.setObjectName(_fromUtf8("apply_btn"))
-		self.blacklisted_sites_list = QtGui.QListWidget(Dialog)
+		self.blacklisted_sites_list = QListWidget(Dialog)
 		self.blacklisted_sites_list.setGeometry(QtCore.QRect(30, 50, 431, 141))
 		self.blacklisted_sites_list.setObjectName(_fromUtf8("blacklisted_sites_list"))
-		self.blacklisted_sites_label = QtGui.QLabel(Dialog)
+		self.blacklisted_sites_label = QLabel(Dialog)
 		self.blacklisted_sites_label.setGeometry(QtCore.QRect(30, 20, 131, 16))
 		font = QtGui.QFont()
 		font.setFamily(_fromUtf8("MS PGothic 12"))
@@ -67,20 +68,20 @@ class Ui_Dialog(QtGui.QWidget):
 		self.blacklisted_sites_label.setFont(font)
 		self.blacklisted_sites_label.setStyleSheet(_fromUtf8("font: 75 12pt \"MS PGothic\" bold;"))
 		self.blacklisted_sites_label.setObjectName(_fromUtf8("blacklisted_sites_label"))
-		self.blacklisted_sites_editfield = QtGui.QLineEdit(Dialog)
+		self.blacklisted_sites_editfield = QLineEdit(Dialog)
 		self.blacklisted_sites_editfield.setGeometry(QtCore.QRect(30, 200, 361, 20))
 		self.blacklisted_sites_editfield.setObjectName(_fromUtf8("blacklisted_sites_editfield"))
-		self.add_blacklisted_sites_btn = QtGui.QPushButton(Dialog)
+		self.add_blacklisted_sites_btn = QPushButton(Dialog)
 		self.add_blacklisted_sites_btn.setGeometry(QtCore.QRect(410, 200, 16, 16))
 		self.add_blacklisted_sites_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.add_blacklisted_sites_btn.setObjectName(_fromUtf8("add_blacklisted_sites_btn"))
-		self.remove_blacklisted_sites_btn = QtGui.QPushButton(Dialog)
+		self.remove_blacklisted_sites_btn = QPushButton(Dialog)
 		self.remove_blacklisted_sites_btn.setGeometry(QtCore.QRect(440, 200, 16, 16))
 		self.remove_blacklisted_sites_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.remove_blacklisted_sites_btn.setObjectName(_fromUtf8("remove_blacklisted_sites_btn"))
-		self.strings_to_modify_label = QtGui.QLabel(Dialog)
+		self.strings_to_modify_label = QLabel(Dialog)
 		self.strings_to_modify_label.setGeometry(QtCore.QRect(30, 240, 161, 21))
 		font = QtGui.QFont()
 		font.setFamily(_fromUtf8("MS PGothic 12"))
@@ -91,35 +92,35 @@ class Ui_Dialog(QtGui.QWidget):
 		self.strings_to_modify_label.setFont(font)
 		self.strings_to_modify_label.setStyleSheet(_fromUtf8("font: 75 12pt \"MS PGothic\" bold;"))
 		self.strings_to_modify_label.setObjectName(_fromUtf8("strings_to_modify_label"))
-		self.strings_to_modify_editfield = QtGui.QLineEdit(Dialog)
+		self.strings_to_modify_editfield = QLineEdit(Dialog)
 		self.strings_to_modify_editfield.setGeometry(QtCore.QRect(30, 380, 201, 20))
 		self.strings_to_modify_editfield.setText(_fromUtf8(""))
 		self.strings_to_modify_editfield.setFrame(False)
-		self.strings_to_modify_editfield.setEchoMode(QtGui.QLineEdit.Normal)
+		self.strings_to_modify_editfield.setEchoMode(QLineEdit.Normal)
 		self.strings_to_modify_editfield.setObjectName(_fromUtf8("strings_to_modify_editfield"))
-		self.strings_modified_editfield = QtGui.QLineEdit(Dialog)
+		self.strings_modified_editfield = QLineEdit(Dialog)
 		self.strings_modified_editfield.setGeometry(QtCore.QRect(260, 380, 131, 20))
 		self.strings_modified_editfield.setText(_fromUtf8(""))
 		self.strings_modified_editfield.setFrame(False)
-		self.strings_modified_editfield.setEchoMode(QtGui.QLineEdit.Normal)
+		self.strings_modified_editfield.setEchoMode(QLineEdit.Normal)
 		self.strings_modified_editfield.setObjectName(_fromUtf8("strings_modified_editfield"))
-		self.remove_strings_to_modify_btn = QtGui.QPushButton(Dialog)
+		self.remove_strings_to_modify_btn = QPushButton(Dialog)
 		self.remove_strings_to_modify_btn.setGeometry(QtCore.QRect(440, 380, 16, 16))
 		self.remove_strings_to_modify_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.remove_strings_to_modify_btn.setObjectName(_fromUtf8("remove_strings_to_modify_btn"))
-		self.add_strings_to_modify_btn = QtGui.QPushButton(Dialog)
+		self.add_strings_to_modify_btn = QPushButton(Dialog)
 		self.add_strings_to_modify_btn.setGeometry(QtCore.QRect(410, 380, 16, 16))
 		self.add_strings_to_modify_btn.setStyleSheet(_fromUtf8("background-color: rgb(0, 98, 255);\n"
 			"color: rgb(255, 255, 255);"))
 		self.add_strings_to_modify_btn.setObjectName(_fromUtf8("add_strings_to_modify_btn"))
-		self.strings_to_modify_list = QtGui.QListWidget(Dialog)
+		self.strings_to_modify_list = QListWidget(Dialog)
 		self.strings_to_modify_list.setGeometry(QtCore.QRect(30, 270, 201, 101))
 		self.strings_to_modify_list.setObjectName(_fromUtf8("strings_to_modify_list"))
-		self.strings_modified_list = QtGui.QListWidget(Dialog)
+		self.strings_modified_list = QListWidget(Dialog)
 		self.strings_modified_list.setGeometry(QtCore.QRect(260, 270, 201, 101))
 		self.strings_modified_list.setObjectName(_fromUtf8("strings_modified_list"))
-		self.label = QtGui.QLabel(Dialog)
+		self.label = QLabel(Dialog)
 		self.label.setGeometry(QtCore.QRect(240, 310, 21, 21))
 		self.label.setStyleSheet(_fromUtf8("font: 75 12pt \"MS PGothic\";"))
 		self.label.setObjectName(_fromUtf8("label"))
@@ -167,7 +168,7 @@ class Ui_Dialog(QtGui.QWidget):
 		if getFirstStrings=="" or getSecondStrings=="":
 			pass
 		else:
-			print getFirstStrings, getSecondStrings
+			print(getFirstStrings, getSecondStrings)
 			self.strings_to_modify_list.addItem(getFirstStrings)
 			self.strings_modified_list.addItem(getSecondStrings)
 			self.strings_to_modify_editfield.clear()
@@ -197,16 +198,16 @@ class Ui_Dialog(QtGui.QWidget):
 			wordlist_dict[str(item.text())] = str(paireditem.text())
 
 		blacklisted_dict = {}
-		for index in xrange(self.blacklisted_sites_list.count()):
+		for index in range(self.blacklisted_sites_list.count()):
 			blacklisted_dict[str(self.blacklisted_sites_list.item(index).text())] = ""
-			print self.blacklisted_sites_list.item(index).text()
+			print(self.blacklisted_sites_list.item(index).text())
 
 		with open('prefs.json','w') as f:
 			f.write(json.dumps({'Wordlist': wordlist_dict, 'Blacklisted_sites':blacklisted_dict}))
 			f.close()
 
 if __name__ == '__main__':
-	app = QtGui.QApplication(sys.argv)
+	app = QApplication(sys.argv)
 	ex = Ui_Dialog()
 	ex.show()
 	sys.exit(app.exec_())
